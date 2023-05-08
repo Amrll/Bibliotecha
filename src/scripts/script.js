0bdab0ef2fca2d3ff2de3491d131824e89defdd2
Amrll <202110934@gordoncollege.edu.ph>
2023-05-09 00:49:32 +0800

    .

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -26,9 +26,19 @@ const swiper = new Swiper(".swiper", {
 
   breakpoints: {
     // when window width is >= 320px
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    
     320: {
       slidesPerView: 1,
       spaceBetween: 20,
+    },
+
+    200: {
+      slidesPerView: 1,
+      spaceBetween: 30,
     },
     // when window width is >= 480px
     480: {
