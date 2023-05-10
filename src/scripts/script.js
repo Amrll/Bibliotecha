0eaa179d593b9d2994ca2b57e659a5e11778eeb3
Amrll <202110934@gordoncollege.edu.ph>
2023-05-11 02:23:58 +0800

    open library api added

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -56,7 +56,7 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-document.querySelectorAll(".category-container a").forEach((tab) => {
+document.querySelectorAll(".search-form a , .category-container a").forEach((tab) => {
   tab.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -113,3 +113,95 @@ function scrollFunction() {
 function scrollToTop() {
   document.documentElement.scrollTop = 0;
 }
+
+//for search engine api using open library
+function getbooks() {
+  const input = document.getElementById("input").value;
+  const outputContainer = document.getElementById("output");
+  outputContainer.innerHTML = "";
+
+  const apiUrl = `http://openlibrary.org/search.json?q=${input}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const books = data.docs.filter((book) => {
+        const isbn = book.isbn ? book.isbn[0] : "";
+        const coverImageUrl = `http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`;
+        return coverImageUrl !== "";
+      });
+
+      let container;
+      let bookCount = 0;
+
+      for (let i = 0; i < books.length; i++) {
+        const book = books[i];
+        const title = book.title;
+        const author = book.author_name ? book.author_name[0] : "Unknown";
+        const isbn = book.isbn ? book.isbn[0] : "";
+        const bookDetailsUrl = `book-details.html?isbn=${isbn}`;
+        const coverImageUrl = `http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`;
+
+        if (bookCount % 4 === 0) {
+          container = document.createElement("div");
+          container.classList.add("books-container");
+          outputContainer.appendChild(container);
+        }
+
+        const bookBox = document.createElement("div");
+        bookBox.classList.add("books-box");
+
+        const coverImage = document.createElement("img");
+        coverImage.src = coverImageUrl;
+        bookBox.appendChild(coverImage);
+
+        const authorName = document.createElement("font");
+        authorName.textContent = author;
+        bookBox.appendChild(authorName);
+
+        const bookTitle = document.createElement("p");
+        bookTitle.textContent = title;
+        bookBox.appendChild(bookTitle);
+
+        const addToCartLink = document.createElement("a");
+        addToCartLink.href = bookDetailsUrl;
+        addToCartLink.textContent = "ADD TO CART";
+        bookBox.appendChild(addToCartLink);
+
+        container.appendChild(bookBox);
+
+        bookCount++;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching books:", error);
+    });
+}
+
+function getBookDetails() {
+  const isbn = getParameterByName("isbn");
+  const apiUrl = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const bookInfo = data[`ISBN:${isbn}`];
+      if (bookInfo) {
+        const title = bookInfo.title;
+        const author = bookInfo.authors ? bookInfo.authors[0].name : "Unknown";
+        const description = bookInfo.notes ? bookInfo.notes.value : "No description available";
+
+        document.getElementById("book-title").textContent = title;
+        document.getElementById("book-author").textContent = author;
+        document.getElementById("book-description").textContent = description;
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching book details:", error);
+    });
+}
+
+
+
+
+
