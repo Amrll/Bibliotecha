e2311790e61b7c39420fa071d6a74427a215a3ce
Amrll <202110934@gordoncollege.edu.ph>
2023-05-12 15:51:42 +0800

    fixes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -201,7 +201,98 @@ function getBookDetails() {
     });
 }
 
+//cart function
 
+const product = [
+  {
+    id: 0,
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1650240624i/58725016.jpg",
+    title: "Elektra",
+    price: 10,
+  },
+  {
+    id: 1,
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1653086275i/58756502.jpg",
+    title: "Jackal",
+    price: 22,
+  },
+  {
+    id: 2,
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1642803780i/59807954.jpg",
+    title: "Other Birds",
+    price: 32,
+  },
+  {
+    id: 3,
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1642705453i/59912428.jpg",
+    title: "Mad Honey",
+    price: 15,
+  },
+  {
+    id: 4,
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654709397i/58537371.jpg",
+    title: "Nightcrawling",
+    price: 16,
+  },
+  {
+    id: 5,
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1663514088i/60209470.jpg",
+    title: "The Winners",
+    price: 22,
+  },
+];
 
+const categories = [...new Set(product.map((item) => 
+  {return item}))]
+  let i=0;
+  document.getElementById('root').innerHTML = categories.map((item) => {
+    var { image, title, price } = item;
+    return `
+      <div class="cart-box">
+        <div class="img-box">
+          <img class='images' src=${image}></img>
+        </div>
+        <div class='bottom'>
+          <p>${title}</p>
+          <h2>$${price}</h2>
+          <button onclick='addtocart(${i++})'>Add to cart</button>
+        </div>
+      </div>
+    `
+  }).join('');
 
+  var cart = [];
 
+  function addtocart(a) {
+    cart.push({...categories[a]});
+    displaycart();
+  }
+
+  function delElement(a) {
+    cart.splice(a, 1);
+    displaycart();
+  }
+  
+  function displaycart(a) {
+    let j = 0, total=0;
+    if (cart.length == 0) {
+      document.getElementById('cartItem').innerHTML = `<h1>Cart is empty</h1>`;
+      document.getElementById('total').innerHTML = "$ "+0+".00";
+    } else {
+      document.getElementById("cartItem").innerHTML = cart.map((items) => {
+        var { image, title, price } = items;
+        total = total + price;
+        document.getElementById('total').innerHTML = "$ "+total+".00";
+        return `
+          <div class="cart-item">
+            <div class="row-img">
+              <img class="rowimg" src=${image}>
+            </div>
+            <p style="font-size: 12px;">${title}</p>
+            <h2 style="font-size: 15px;">$${price}.00</h2>
+            <i class='fa fa-trash' onclick='delElement(${j++})'></i>
+          </div>
+        `;
+      }).join('');
+    }
+  }
